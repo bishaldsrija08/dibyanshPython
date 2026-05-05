33484783c3c95807869b4c625fb2044478dc3b4b
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-05-05 06:38:45 +0545

    first commit

---
+++ day 1/day1.py
@@ -0,0 +1,24 @@
+# Syntax, Comments and Statement in python
+
+# Syntax is the set of rules that defines how a Python program will be written and interpreted. It includes rules for writing code, such as how to define variables, how to write functions, and how to use control structures like loops and conditionals.
+
+# Comments are lines in a program that are not executed by the interpreter. They are used to explain the code and make it easier to understand. In Python, comments start with the # symbol.
+
+""" This is a multi-line comment in Python. It can be used to provide more detailed explanations or to comment out blocks of code. """
+
+# Statements are instructions that the Python interpreter can execute. They can be simple, like assigning a value to a variable, or more complex, like defining a function or creating a loop.
+
+print("Hello, World!")  # This is a statement that prints "Hello, World!" to the console.
+print("This is a comment")  # This is another statement that prints "This is a comment" to the console.
+print("I am learning Python")  # This is a statement that prints "I am learning Python" to the console.
+print("Python is great!")
+print("I love coding in Python!")
+print("Python is a versatile language!")
+print("Python is used in many fields, such as web development, data science, and artificial intelligence!")
+print("Python has a large and supportive community!")
+print("Python is a great language for beginners and experienced programmers alike!")
+print(9+10)
+print(9*10)
+print(90/10)
+print(19-10)
+print(234)
