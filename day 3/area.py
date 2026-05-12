106595135b4fbcb336cda82c469a2d1bafed911f
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-05-12 06:21:49 +0545

    Add area calculation, input/output examples, and subtraction functionality

---
+++ day 3/area.py
@@ -0,0 +1,7 @@
+# Area of rectangle. [length * breadth]
+
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+
+area = length * breadth
+print("The area of the rectangle is:", area)
---
+++ day 3/inputOutput.py
@@ -0,0 +1,11 @@
+# Output
+print("Hello, World!")  # Alphaneumric output
+print(123 + 2)  # Numeric output
+print(3.14)  # Float output
+
+# age = int(input("Please enter your age: "))  # Input from user
+# print("You entered: ", age + 2)  # Output the input received from user
+
+
+print("11"+"11")  # String concatenation
+print(11+11)  # Numeric addition
