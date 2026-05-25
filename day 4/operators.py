8320e088cc9864ca41f13a1ce63ae6c697b366e2
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-05-25 06:49:37 +0545

    Add arithmetic, assignment, comparison, and logical operators examples

---
+++ day 4/operators.py
@@ -0,0 +1,35 @@
+# Arithmetic operators
+
+print(2 + 3) # Addition
+print(5 - 2) # Subtraction
+print(4 * 3) # Multiplication
+print(10 / 2) # Division
+print(13 % 5) # Modulus - To get the remainder of a division
+print(4 ** 5) # Exponentiation - 4x4x4x4x4
+
+a=5
+b=6
+print(a + b) # Addition
+
+# Assignment operators
+num1 = 10
+print(num1)
+
+# Comparison operators
+print(5==5) # Equal to
+print(5!=3) # Not equal to
+print(5>3) # Greater than
+print(3>5) # Greater than
+print(5>=5) # Greater than or equal to
+print(3<=5) # Less than or equal to
+print("------------")
+
+# Logical operators
+print(True and False) # Logical AND
+print(True or False) # Logical OR
+print(not True) # Logical NOT
+
+print("------------")
+print(5 > 3 and 2 < 4) # True and True -> True
+print(5 > 3 or 2 > 4) # True or False -> True
+print(not (5 > 3)) # not True -> False
