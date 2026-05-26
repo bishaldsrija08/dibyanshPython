1cf79ea15e661ef8c8d1aa6559cc84782e84ce13
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-05-26 06:45:32 +0545

    Add variable examples and type demonstrations for integers, floats, strings, booleans, and print functionality

---
+++ day 6/dt.py
@@ -0,0 +1,15 @@
+# Integer -> int
+age = 25
+print(type(age))
+
+# Float -> float
+pi = 3.14
+print(type(pi))
+
+# String -> str
+name = "Bishal Rijal"
+print(type(name))
+
+# Boolean -> bool
+is_student = True
+print(type(is_student))
---
+++ day 6/var.py
@@ -0,0 +1,19 @@
+x = 6
+y = 5
+y = 10
+print(x + y)
+
+name = "Bishal Rijal"
+print(name)
+
+_name = "Bishal Rijal"
+print(_name)
+
+first_name = "Bishal"
+
+# print = 6
+# print(print)
+
+
+x = "Python is awesome"
+print(x)
