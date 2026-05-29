dd191fe787b7c945acbbb40c91af3d6088d45423
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-05-29 12:32:26 +0545

    Simple Project I (Mini Calculator in Python)

---
+++ projects/simple_calc.py
@@ -0,0 +1,17 @@
+# Simple Project I (Mini Calculator in Python)
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+add = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+exponent = num1 ** num2
+rem = num1 % num2
+print("Addition: ", add)
+print("Subtraction: ", subtract)
+print("Multiplication: ", multiply)
+print("Division: ", divide)
+print("Exponent: ", exponent)
+print("Remainder: ", rem)
