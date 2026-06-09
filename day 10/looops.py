0b0995362a3273e48c1562b0efaf5bf75cdc591f
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-06-09 06:29:01 +0545

    Add for loop examples demonstrating printing names, natural numbers, even/odd numbers, squares, and cubes

---
+++ day 10/looops.py
@@ -0,0 +1,39 @@
+# For loops
+
+"""
+for var in iterable(initialize, condition, update):
+    # do something with var
+"""
+
+# Print your name 10 times.
+for i in range(1, 11,1):
+    print("Bishal Rijal")
+    
+print()
+
+# Print the first 10 natural numbers.
+for i in range(1, 11, 1):
+    print(i)
+
+print()
+
+# Print the first 10 even numbers.
+for i in range(2, 21, 2):
+    print(i)
+
+print()
+
+# Print the first 10 odd numbers.
+for i in range(1, 20, 2):
+    print(i)
+
+print()
+
+# Print the first 10 square numbers.
+for i in range(1, 11, 1):
+    print(i**2)
+
+# Print the first 10 cube numbers.
+for i in range(1, 11, 1):
+    print(i**3)
+# 1x1x1, 2x2x2, 3x3x3, 4x4x4, 5x5x5, 6x6x6, 7x7x7, 8x8x8, 9x9x9, 10x10x10
