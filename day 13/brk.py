85f24cb0f1432d2001c907b37eaa901138b782ce
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-06-19 06:34:58 +0545

    Add examples of for loops for printing even and odd numbers

---
+++ day 13/brk.py
@@ -0,0 +1,12 @@
+for i in range(1, 11, 1):
+    if i ==5:
+        break
+    print(i)
+
+
+i = 1
+while i <= 10:
+    if i == 5:
+        break
+    print(i)
+    i += 1
---
+++ day 13/cont.py
@@ -0,0 +1,12 @@
+# Even numbers
+for i in range(1, 11, 1):
+    if i % 2 != 0:
+        continue
+    print(i)
+    
+
+# Odd numbers
+for i in range(1, 11, 1):
+    if i % 2 == 0:
+        continue
+    print(i)
